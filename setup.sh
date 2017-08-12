6c270853df7b007173d584dfc880b8160cb577de
Ben Lowry <benlowry@outlook.com>
2017-08-12 17:39:41 -0600

    Startup

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ npm install pm2 -g
 pm2 start main.js --name pgweb-bookmarks
 pm2 start pgweb_linux_amd64 --name pgweb -- --skip-open --sessions --prefix=$PGWEB_PREFIX --listen=$PGWEB_PORT
 pm2 save
-sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v8.1.4/bin /home/ubuntu/.nvm/versions/node/v8.1.4/lib/node_modules/pm2/bin/pm2 startup ubntu -u ubuntu --hp /home/ubuntu
+sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v8.1.4/bin /home/ubuntu/.nvm/versions/node/v8.1.4/lib/node_modules/pm2/bin/pm2 startup ubuntu -u ubuntu --hp /home/ubuntu

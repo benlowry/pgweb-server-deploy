f8311fd051e2e6d9bb3896a0deeec98526505308
Ben Lowry <benlowry@outlook.com>
2017-08-12 17:31:32 -0600

    Source

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+source $HOME/.bashrc
 nvm install 8.1.4
 npm install pm2 -g
 pm2 start main.js --name pgweb-server-deploy

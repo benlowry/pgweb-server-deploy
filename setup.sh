a69904195615eb61c2e309da4ffcabcf22286cd8
Ben Lowry <benlowry@outlook.com>
2017-08-12 17:28:36 -0600

    Less

--- setup.sh
+++ setup.sh
@@ -1,14 +1,8 @@
-# install nodejs via nvm and pm2
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install 8.1.4
 npm install pm2
-
-# start the server and pgweb
-cd $HOME
-git clone https://github.com/benlowry/pgweb-server-deploy
-cd pgweb-server-deploy
 pm2 start main.js --name pgweb-server-deploy
 pm2 start pgweb_linux_amd64 --skip-open --sessions --prefix=$PGWEB_PREFIX --listen=$PGWEB_PORT
 pm2 save
